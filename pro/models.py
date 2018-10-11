59e02ff6fb1992bd5f1aba8f507e64614a17972c
Owenmur21 <owenmuriithi@gmail.com>
2018-10-11 22:56:03 +0300

    Creates Profile class

--- pro/models.py
+++ pro/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+
+class Profile(models.Model):
+    """
+    Class that contains Profile details
+    """
+    profile_pic = models.ImageField(upload_to = 'images/', blank=True)
+    bio = models.TextField()
+    contact = models.CharField(max_length = 30, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+
