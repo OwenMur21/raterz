7ed6c2c033f17931f03b8d759a2fa23988f9fc62
Owenmur21 <owenmuriithi@gmail.com>
2018-10-11 23:01:36 +0300

    Creates Project class

--- pro/models.py
+++ pro/models.py
@@ -12,4 +12,22 @@ class Profile(models.Model):
     contact = models.CharField(max_length = 30, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
+    def __str__(self):
+                return self.bio
+
+class Project(models.Model):
+    """
+    Class that contains Project details
+    """
+    title = models.TextField()
+    landing_page = models.ImageField(upload_to = 'images/')
+    description = models.TextField()
+    link = models.CharField(max_length = 30)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+                return self.bio
+
+
 
