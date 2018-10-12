fa2f54f7c83a314c91f670c774bc5b90e2687445
Owenmur21 <owenmuriithi@gmail.com>
2018-10-12 11:20:16 +0300

    Adds signals and receiver

--- pro/models.py
+++ pro/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 
@@ -11,6 +13,17 @@ class Profile(models.Model):
     bio = models.TextField()
     contact = models.CharField(max_length = 30, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    post_save.connect(save_user_profile, sender=User)
 
     def __str__(self):
                 return self.bio
