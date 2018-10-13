bb4f942affc010ce0148209891ee58200ac79afd
Owenmur21 <owenmuriithi@gmail.com>
2018-10-14 00:45:26 +0300

    Creates search template and view function

--- pro/views.py
+++ pro/views.py
@@ -75,4 +75,17 @@ def profile(request, user_id):
     users = User.objects.get(id=user_id)
     return render(request, 'profile/profile.html', locals())
 
+def search_results(request):
+
+    if 'pro' in request.GET and request.GET["pro"]:
+        search_term = request.GET.get("pro")
+        searched_projects = Project.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"pros": searched_projects})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+
 
